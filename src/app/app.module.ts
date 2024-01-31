a693731a87c404d00f2d09627c6e0843c104d78f
utshow-rehman <utshowrehman2@gmail.com>
2024-01-31 11:07:24 +0600

    sidebar design done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
+import { SidebarComponent } from './Component/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
