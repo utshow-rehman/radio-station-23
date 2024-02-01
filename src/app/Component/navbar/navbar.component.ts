c11cbefe2dcd3221edb62d72985b946a0a9e7789
utshow-rehman <utshowrehman2@gmail.com>
2024-02-01 12:54:47 +0600

    dark mode done

--- src/app/Component/navbar/navbar.component.ts
+++ src/app/Component/navbar/navbar.component.ts
@@ -17,4 +17,19 @@ export class NavbarComponent {
   toggleDrawer(): void {
     this.drawerService.toggleDrawer();
   }
+  enableDarkmode() {
+    this.checked = this.checked ? false : true;
+    console.log(this.checked, 'checked');
+    if (this.checked) {
+      document.documentElement.style.setProperty('--bg-body', '#242a30');
+      document.documentElement.style.setProperty('--card-bg', '#404142');
+      document.documentElement.style.setProperty('--card-color', '#fff');
+      document.documentElement.style.setProperty('--color-gray', '#bcc0c4');
+    } else {
+      document.documentElement.style.setProperty('--bg-body', '#f1f1f1');
+      document.documentElement.style.setProperty('--card-bg', '#fff');
+      document.documentElement.style.setProperty('--card-color', '#212529');
+      document.documentElement.style.setProperty('--color-gray', 'gray');
+    }
+  }
 }
